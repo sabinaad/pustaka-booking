2adda974ba59efe2635acf76243212c6d1aae2bf
sabinaad <adindasabina2003@gmail.com>
2024-04-05 16:07:01 +0700

    tugas p4

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Sabina</li> 
+        <li>Nama Belakang : Adinda</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan Nangka 2, Blok D34, No.71 </li>
+    </ul> 
+     
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Depok</li> 
+    </ul> 
+ 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Tidur</li> 
+        <li>Tidursiang</li> 
+    </ul> 
+</section>
